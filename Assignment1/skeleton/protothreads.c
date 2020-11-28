8e783c062bbc9898844093c018cbd94ab4433d06
PriyankaSethi495 <73972804+PriyankaSethi495@users.noreply.github.com>
2020-11-28 20:25:04 +0530

    Update protothreads.c

--- Assignment1/skeleton/protothreads.c
+++ Assignment1/skeleton/protothreads.c
@@ -5,8 +5,6 @@
 #include "dev/leds.h"
 
 PROCESS(protothread1, "Protothread Process 1");
-PROCESS(protothread2, "Protothread Process 2");
-PROCESS(protothread3, "Protothread Process 3");
 AUTOSTART_PROCESSES(&protothread1);
 
 PROCESS_THREAD(protothread1, ev, data) {
@@ -15,35 +13,13 @@ PROCESS_THREAD(protothread1, ev, data) {
 	printf("Protothread 1!\n");
 
 	SENSORS_ACTIVATE(button_sensor);
+	SENSORS_ACTIVATE(light_sensor);
 
 	while (1) {
 		PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
 		printf("Light sensor reads value %d\n", light_sensor.value(0));
 		leds_toggle(LEDS_ALL);
-		process_start(&protothread3,NULL);
 	}
 
-	PROCESS_END();
-}
-
-PROCESS_THREAD(protothread2, ev, data) {
-	PROCESS_BEGIN();
-
-	printf("Protothread 2!\n");
-        while(1) {
-    	  printf("Protothread 2 is running");
-    	  PROCESS_PAUSE();
-    }	
-	PROCESS_END();
-}
-
-PROCESS_THREAD(protothread3, ev, data) {
-	PROCESS_BEGIN();
-
-	printf("Protothread 3!\n");
-        while(1) {
-    	  printf("Protothread 3 is running");
-    	  PROCESS_PAUSE();
-    }	
 	PROCESS_END();
 }
