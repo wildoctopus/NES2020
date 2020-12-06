1469f5d171d3197ae3168700d6471b6ea2827051
Panneer Selvam <rmd.selvam@gmail.com>
2020-12-06 13:50:20 +0100

    added comments

--- Assignment2/solution/energy.c
+++ Assignment2/solution/energy.c
@@ -25,29 +25,48 @@ PROCESS_THREAD(btn_pt, ev, data) {
     
     while(1) {
         PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
-        leds_toggle(LEDS_GREEN);
-
+        // Check change_time method to undeerstand the time_on and time_off values
         switch (duty_cycle) {
             case 10:
                 duty_cycle = 50;
-                time_on = 0.002;
-                time_off = 0.198;
+                time_on = 0.01;
+                time_off = 0.01;
+                printf("switching to %d percent\n", duty_cycle);
                 break;
             case 50:
                 duty_cycle = 90;
-                time_on = 0.01;
-                time_off = 0.01;
+                time_on = 0.018;
+                time_off = 0.002;
+                printf("switching to %d percent\n", duty_cycle);
                 break;
             case 90:
                 duty_cycle = 10;
-                time_on = 0.018;
-                time_off = 0.002;
+                time_on = 0.002;
+                time_off = 0.198;
+                printf("switching to %d percent\n", duty_cycle);
                 break;
         }
+        // change_time(duty_cycle);
         
     }
     
     PROCESS_END();
+}
+
+// TODO: Implement this method properly, could not print float values here to check the correctness
+void change_time(int brightness_level) {
+    // Below formulas are  applied to calculate the time_on and time_off intervals
+    // 1) Frequnecy is 50 hz
+    // 2) Period ( T ) =  1 / f ;
+    // 3) Period ( T ) = time_on + time_off;
+    // 4) Duty_cycle = ( time_on / T ) * 100 ; => time_on = (Duty_cycle / 100 ) * T ;
+
+    int frequency = 50;
+    float period = 1 / frequency ;
+    // printf("period %lf \n", period );
+
+    time_on = (brightness_level / 100 ) * period;
+    time_off = period - time_on;
 }
 
 PROCESS_THREAD(led_pt, ev, data) {
@@ -57,12 +76,14 @@ PROCESS_THREAD(led_pt, ev, data) {
     etimer_set(&timer, CLOCK_SECOND);
    while(1) {
      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-     leds_toggle(LEDS_RED);
+     // leds_toggle(LEDS_RED);
      if( i== 0 ){
          i = 1;
+         leds_on(LEDS_RED);
          etimer_set(&timer, CLOCK_SECOND * time_on);
      }else if(i == 1){
         i = 0;
+        leds_off(LEDS_RED);
         etimer_set(&timer, CLOCK_SECOND * time_off);
      }
    }
