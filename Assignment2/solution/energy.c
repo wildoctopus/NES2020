043b8a046891ce62c1b4b0b5b57193cdbe56277a
Panneer Selvam <rmd.selvam@gmail.com>
2020-12-13 13:11:30 +0100

    All doen and optimized

--- Assignment2/solution/energy.c
+++ Assignment2/solution/energy.c
@@ -26,68 +26,38 @@ PROCESS_THREAD(btn_pt, ev, data) {
     
     while(1) {
         PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
-        // Check change_time method to undeerstand the time_on and time_off values
-        switch (duty_cycle) {
-            case 10:
-            	// TODO: Remove the Hardcoding by implementing the change_time method
-                time_on = change_time(duty_cycle);
-                time_off = period - time_on;
-                
-                duty_cycle = 50;
-                printf("switching to %d percent\n", duty_cycle);
-                
-                break;
-            case 50:
-                
-                time_on = change_time(duty_cycle);
-                time_off = period - time_on;
-                
-                duty_cycle = 90;
-                printf("switching to %d percent\n", duty_cycle);
-                
-                break;
-            case 90:
-                
-                time_on = change_time(duty_cycle);
-                time_off = period - time_on;
-                
-                duty_cycle = 10;
-                printf("switching to %d percent\n", duty_cycle);
-                
-                break;
-        }
-                // change_time(duty_cycle);
-        
-        
+        duty_cycle = (duty_cycle == 90)? 10: (duty_cycle + 40);
+        change_time();
     }
     
     PROCESS_END();
 }
 
-// TODO: Implement this method properly, could not print float values here to check the correctness
-float change_time(int brightness_level) {
+void change_time() {
     // Below formulas are  applied to calculate the time_on and time_off intervals
     // 1) Frequnecy is 50 hz
     // 2) Period ( T ) =  1 / f ;
     // 3) Period ( T ) = time_on + time_off;
     // 4) Duty_cycle = ( time_on / T ) * 100 ; => time_on = (Duty_cycle / 100 ) * T ;
 
-    int frequency = 50;
+    float frequency = 50;
     float period = 1 / frequency ;
-    // printf("period %lf \n", period );
 
-    float on_time = (brightness_level / 100 ) * period;
-    return on_time;
+    time_on = (float) (duty_cycle* period) / 100;
+    time_off = (float)period - time_on;
 }
 
 PROCESS_THREAD(led_pt, ev, data) {
     static struct etimer timer;
     static int i = 0;
     PROCESS_BEGIN();
+
     etimer_set(&timer, CLOCK_SECOND);
+
+    printf("starting with Initial duty cycle %d\n", duty_cycle);
+
    while(1) {
      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-     // leds_toggle(LEDS_RED);
      if( i== 0 ){
          i = 1;
          leds_on(LEDS_RED);
@@ -101,10 +71,6 @@ PROCESS_THREAD(led_pt, ev, data) {
 
     PROCESS_END();
 }
-
-// static unsigned long to_seconds(uint64_t time) {
-//   return (unsigned long)(time / RTIMER_SECOND);
-// }
 
 PROCESS_THREAD(energy_pt, ev, data) {
     PROCESS_BEGIN();
